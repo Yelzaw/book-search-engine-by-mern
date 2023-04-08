00c870e56431da01af3305817f32f0451cce2a22
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-04-08 13:20:04 -0300

    solving errors

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -12,7 +12,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [login] = useMutation(LOGIN_USER);
+  const [login, {error, data}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -46,7 +46,7 @@ const LoginForm = () => {
       // Auth.login(token);
     } catch (err) {
       console.error(err);
-      // setShowAlert(true);
+      setShowAlert(true);
     }
 
     setUserFormData({
