3d0ca471f7f99ca73ffef1b1d5127e9e8a3bd6af
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-04-08 13:23:36 -0300

    solving errors

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -47,7 +47,7 @@ const SignupForm = () => {
       // Auth.login(token);
     } catch (err) {
       console.error(err);
-      // setShowAlert(true);
+      setShowAlert(true);
     }
 
     setUserFormData({
